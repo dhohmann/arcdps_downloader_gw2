605ba9ff670d7b2696efce7d3b99b7385c05cbae
Daniel Hohmann <dtwin95@gmx.de>
2022-12-29 23:27:18 +0100

    Added log when game is closed

--- src/main/java/com/gw2/arcdpsdownloadergw2/action/impl/StartGame.java
+++ src/main/java/com/gw2/arcdpsdownloadergw2/action/impl/StartGame.java
@@ -1,7 +1,6 @@
 package com.gw2.arcdpsdownloadergw2.action.impl;
 
 import java.io.File;
-import java.io.IOException;
 
 import com.gw2.arcdpsdownloadergw2.Utils;
 import com.gw2.arcdpsdownloadergw2.action.Action;
@@ -11,6 +10,10 @@ import com.gw2.arcdpsdownloadergw2.action.Action;
  */
 public class StartGame implements Action {
 
+    private static final String CLI_PARAM_AUTOLOGIN = "-autologin";
+
+    private static Thread gameThread = null;
+
     @Override
     public void execute() {
         File gameFile = Utils.getGW2Executable();
@@ -18,14 +21,32 @@ public class StartGame implements Action {
             System.out.println("Could not start game: " + "Please open settings to specify game directory.");
             return;
         }
-
-        String executablePath = gameFile.toPath().toAbsolutePath().toString();
-        try {
-            System.out.println("Starting game...");
-            Runtime.getRuntime().exec(executablePath);
-        } catch (IOException e) {
-            System.out.println("Could not start game: " + e.getMessage());
+        if (gameThread != null) {
+            System.out.println("Game is already starting or running.");
+            return;
         }
+        String executablePath = gameFile.toPath().toAbsolutePath().toString();
+        ProcessBuilder processBuilder = new ProcessBuilder(executablePath, CLI_PARAM_AUTOLOGIN);
+
+        gameThread = new Thread(() -> {
+            try {
+                System.out.println("Starting game...");
+                Process process = processBuilder.start();
+                int result = process.waitFor();
+                if (result == 0) {
+                    System.out.println("Game closed.");
+                } else {
+                    System.out.println("Game closed unexpected. Code: " + result);
+                }
+            } catch (Exception e) {
+                System.out.println("Could not start game: " + e.getMessage());
+            } catch (Throwable e) {
+                System.out.println("An error occured: " + e.getMessage());
+            }
+            gameThread = null;
+        });
+        gameThread.start();
+
     }
 
 }
